f857a33b135897bc9c31be503fc4766371dddea0
Amiya Ranjan Das <97892336+AmiyaRanjanDas@users.noreply.github.com>
2024-03-06 18:23:01 +0530

    Update 5

--- script.js
+++ script.js
@@ -126,65 +126,6 @@ document.querySelector(".sec4 h3").addEventListener("mouseleave", function () {
 });
 
 // sec5
-// let target = 0;
-// let current = 0;
-// const ease = 0.075;
-// const slider = document.querySelector(".slider");
-// const sliderWrapper = document.querySelector(".slider-wrapper");
-// const slides = document.querySelectorAll(".slide");
-// const sec5 = document.querySelector(".sec5");
-// let maxScroll;
-
-// function lerp(start, end, factor) {
-//     return (start + (end - start) * factor);
-// }
-
-// function updateScaleAndPosition() {
-//     slides.forEach((slide) => {
-//         const rect = slide.getBoundingClientRect();
-//         const centerPosition = (rect.left + rect.right) / 2;
-//         const distanceFromCenter = centerPosition - window.innerWidth / 2;
-
-//         let scale, offsetX;
-//         if (distanceFromCenter > 0) {
-//             scale = Math.min(1.75, 1 + distanceFromCenter / window.innerWidth);
-//             offsetX = (scale - 1) * 300;
-//         } else {
-//             scale = Math.max(0.5, 1 - Math.abs(distanceFromCenter) / window.innerWidth);
-//             offsetX = 0;
-//         }
-//         gsap.set(slide, {
-//             scale: scale,
-//             x: offsetX
-//         });
-//     });
-// }
-
-// function update() {
-//     current = lerp(current, target, ease);
-//     gsap.set(".slider-wrapper", {
-//         x: -current,
-//     })
-//     updateScaleAndPosition();
-//     requestAnimationFrame(update);
-// }
-
-// function updateMaxScroll() {
-//     maxScroll = sliderWrapper.offsetWidth - sec5.offsetWidth;
-// }
-
-// sec5.addEventListener("wheel", (e) => {
-//     target += e.deltaY;
-//     target = Math.max(0, target);
-//     target = Math.min(maxScroll, target);
-// });
-
-// window.addEventListener("resize", () => {
-//     updateMaxScroll();
-// });
-
-// updateMaxScroll();
-// update();
 
 const track = document.querySelector(".img-track");
 const sec5 = document.querySelector(".sec5");
